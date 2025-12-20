14611fef1d1e11d77b75c6da7e2f2f6fb6babc39
Gustavo Joel De Souza <cvzwkk@gmail.com>
2025-12-20 09:37:01 -0300

    Update hma.py

--- api/hma/hma.py
+++ api/hma/hma.py
@@ -34,8 +34,8 @@ ngrok.set_auth_token(NGROK_AUTHTOKEN)
 # EXCHANGES & CONSTANTS
 # =========================
 ORDERBOOK_APIS = {
-    "Coinbase": "https://api.exchange.coinbase.com/products/BTC-USD/book?level=2",
-    "Kraken": "https://api.kraken.com/0/public/Depth?pair=XBTUSD&count=10",
+    #"Coinbase": "https://api.exchange.coinbase.com/products/BTC-USD/book?level=2",
+    #"Kraken": "https://api.kraken.com/0/public/Depth?pair=XBTUSD&count=10",
     #"Bitstamp": "https://www.bitstamp.net/api/v2/order_book/btcusd/",
     "Bitfinex": "https://api.bitfinex.com/v1/book/btcusd"
 }
