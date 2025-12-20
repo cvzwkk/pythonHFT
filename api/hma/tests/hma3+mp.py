53179aee7ec9d89429f014fa398ac1f977eeecfe
Gustavo Joel De Souza <cvzwkk@gmail.com>
2025-12-20 14:26:09 -0300

    Update hma3+mp.py

--- api/hma/tests/hma3+mp.py
+++ api/hma/tests/hma3+mp.py
@@ -889,6 +889,45 @@ renderAggTable = function () {
 };
 </script>
 
+<div id="divResize4" style="position: absolute; left: 0%; top: 68%; height: 100%; width: 100%">
+
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container" style="height:100%;width:100%">
+  <div class="tradingview-widget-container__widget" style="height:calc(100% - 32px);width:100%"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/BTCUSD/?exchange=BITFINEX" rel="noopener nofollow" target="_blank"><span class="blue-text">Bitcoin price</span></a><span class="trademark"> by TradingView</span></div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js" async>
+  {
+  "allow_symbol_change": true,
+  "calendar": false,
+  "details": true,
+  "hide_side_toolbar": false,
+  "hide_top_toolbar": false,
+  "hide_legend": false,
+  "hide_volume": false,
+  "hotlist": false,
+  "interval": "1",
+  "locale": "en",
+  "save_image": true,
+  "style": "8",
+  "symbol": "BITFINEX:BTCUSD",
+  "theme": "dark",
+  "timezone": "Etc/UTC",
+  "backgroundColor": "#0F0F0F",
+  "gridColor": "rgba(242, 242, 242, 0.06)",
+  "watchlist": [],
+  "withdateranges": true,
+  "compareSymbols": [],
+  "studies": [
+    "STD;VWAP",
+    "STD;Smoothed%1Moving%1Average"
+  ],
+  "autosize": true
+}
+  </script>
+</div>
+<!-- TradingView Widget END -->
+</div>
+
 </body>
 </html>
 """
