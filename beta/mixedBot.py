d46dcc7b4f226f4ec3a9bae41f2f5d3002d1e641
Gustavo Joel De Souza <cvzwkk@gmail.com>
2025-12-23 13:40:00 -0300

    Update mixedBot.py

--- beta/mixedBot.py
+++ beta/mixedBot.py
@@ -24,8 +24,8 @@ nest_asyncio.apply()
 # =========================
 # NGROK AUTH KEY
 # =========================
-NGROK_AUTH_TOKEN = os.getenv("37FbPTgCns8TUkTfiqKODN85Fst_6g5utZszXCzwQ2cNDSs78")
-ngrok.set_auth_token(NGROK_AUTH_TOKEN)
+NGROK_AUTHTOKEN = "37FbPTgCns8TUkTfiqKODN85Fst_6g5utZszXCzwQ2cNDSs78"
+ngrok.set_auth_token(NGROK_AUTHTOKEN)
 
 # =========================
 # UTILS
