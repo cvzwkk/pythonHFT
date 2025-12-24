9fffa10f05688a0962193ce756acc0e984a096a9
Gustavo Joel De Souza <cvzwkk@gmail.com>
2025-12-24 01:14:40 -0300

    Update builld.sh

--- beta/AVX-AVX2/builld.sh
+++ beta/AVX-AVX2/builld.sh
@@ -1,5 +1,5 @@
 sudo apt install libwebsockets-dev libssl-dev
 gcc bitfinex_paper_bot.c -O3 -march=native \
-    -mavx -mavx2 -mfma \
+    -mavx -mavx2 -mfma -mavx512f -mavx512dq \
     -lwebsockets -lssl -lcrypto -lm \
     -o bitfinex_bot
